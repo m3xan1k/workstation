81f16eedf15393c33f80186aa6258d500ac444bd
Shevtsov Sergey <dev.serzh@gmail.com>
2020-06-15 11:39:31 +0300

    ansible installation

--- third-party.sh
+++ third-party.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install software-properties-common
 sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora -y
+sudo add-apt-repository ppa:ansible/ansible -y
 sudo add-apt-repository ppa:atareao/atareao -y
 sudo apt update
-sudo apt install my-weather-indicator
-sudo apt upgrade firefox
+sudo apt install my-weather-indicator ansible -y
+sudo apt-get --only-upgrade install firefox -y
 
