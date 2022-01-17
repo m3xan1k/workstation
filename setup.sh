a6574861d311cf617fd9e1072cceb28b699f514c
Sergey Shevtsov <35059951+m3xan1k@users.noreply.github.com>
2022-01-17 20:51:26 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,10 +16,12 @@ sudo apt update
 sudo apt install -y curl software-properties-gtk ubuntu-restricted-extras filezilla vim neovim \
     gnome-shell-pomodoro tilix keepass2 obs-studio virtualbox vlc transmission-qt apt-transport-https \
     ca-certificates build-essential software-properties-common python3.10 python3.10-dev fonts-firacode \
-    gnome-tweaks network-manager-l2tp network-manager-l2tp-gnome python3-pip
+    gnome-tweaks network-manager-l2tp network-manager-l2tp-gnome python3-pip gnupg2
 
 # install docker
-sudo apt install -y docker.io
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt install docker-ce
 sudo usermod -aG docker ${USER}
 
 # install compose
